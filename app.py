45b6dc2d38c6183bd14ebb30341237bb48850a22
atharkn7 <athar.kn7@gmail.com>
2025-04-01 05:25:40 -0400

    Configuring Session

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, request, redirect
 from flask_session import Session
 
-#Configuring session
-
-
+#Configure app
 app = Flask(__name__)
+
+#Configuring session to use file system
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Home Directory asking to Login
 @app.route("/", methods=["GET", "POST"])
@@ -12,6 +15,7 @@ def index():
     if request.method == "GET":
         return render_template("login.html")
     else:
+        # Post
         return render_template("login.html")
 
 # Registering new user
