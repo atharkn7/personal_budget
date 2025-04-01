ae380d81c39b5282bdde8aba5831f9fe7c2116b8
atharkn7 <athar.kn7@gmail.com>
2025-04-01 05:05:32 -0400

    login & register layout designed

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_session import Session
+
+#Configuring session
 
 
 app = Flask(__name__)
 
-@app.route("/")
+# Home Directory asking to Login
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
+
+# Registering new user
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    else: 
+        return redirect("/")
